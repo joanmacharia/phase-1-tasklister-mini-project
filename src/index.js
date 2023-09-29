742e8d8e23ccbfb0b62939d1f4f6d2849fd378b3
Joan Macharia <joan.macharia@students.moringa>
2023-09-29 09:03:15 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+  e.preventDefault()
+
+  const myForm = document.getElementById('create-task-form')
+  myForm.addEventListener('submit', (e)  => {
+    e.preventDefault()
+
+    const newTask = document.getElementById('new-task-description')
+
+    const taskList = document.getElementById('tasks')
+
+    const newTaskItem = document.createElement('li')
+    newTaskItem.innerText = newTask.value
+    taskList.appendChild(newTaskItem)
+    e.target.reset()
+
+  })
+  
 });
